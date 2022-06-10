ac06afa55cefc33e48ee062d3df1adf3e9cedb4d
andresja75 <andresjadx@gmail.com>
2022-06-10 02:04:23 +0200

    ejercicios seccion 1

---
+++ src/ejercicios/tema1/Ejercicios1.java
@@ -0,0 +1,32 @@
+package ejercicios.tema1;
+
+public class Ejercicios1 {
+
+    public static void main(String args[])
+    {
+        int numero1;
+        long numero2;
+        float numero3;
+        double numero4;
+        boolean logic_value;
+        String str;
+
+        numero1 = -13451;
+        numero2 = 94259;
+
+        numero3 = 3.3412f;
+        numero4 = -2131.23123;
+
+        logic_value = true;
+        str = "hola que tal";
+
+        System.out.println("int: " + numero1);
+        System.out.println("long: " + numero2);
+
+        System.out.println("float: " + numero3);
+        System.out.println("double: " + numero4);
+
+        System.out.println("boolean: " + logic_value);
+        System.out.println("String: " + str);
+    }
+}
